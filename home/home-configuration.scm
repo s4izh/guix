ab1d3e3ddb21b7a0405728d14a9a218b293c416b
s4izh <sergio.sanz.martinez@proton.me>
2024-05-05 16:47:34 +0200

    vm: setup

---
+++ home/home-configuration.scm
@@ -0,0 +1,45 @@
+;; This "home-environment" file can be passed to 'guix home reconfigure'
+;; to reproduce the content of your profile.  This is "symbolic": it only
+;; specifies package names.  To reproduce the exact same profile, you also
+;; need to capture the channels being used, as returned by "guix describe".
+;; See the "Replicating Guix" section in the manual.
+
+(use-modules (gnu home)
+             (gnu packages)
+             (gnu services)
+             (guix gexp)
+             (gnu home services shells))
+
+(home-environment
+  ;; Below is the list of packages that will show up in your
+  ;; Home profile, under ~/.guix-home/profile.
+  (packages (specifications->packages (list "ripgrep"
+                                            "clang"
+                                            "tmux"
+                                            "tree-sitter-c"
+                                            "tree-sitter-cpp"
+                                            "tree-sitter-lua"
+                                            "tree-sitter-scheme"
+                                            "gcc-toolchain@11"
+                                            "neovim"
+                                            "neofetch"
+                                            "emacs-geiser-guile"
+                                            "emacs-geiser"
+                                            "guile"
+                                            "emacs"
+                                            "nss-certs"
+                                            "git"
+                                            "vim")))
+
+  ;; Below is the list of Home services.  To search for available
+  ;; services, run 'guix home search KEYWORD' in a terminal.
+  (services
+   (list (service home-bash-service-type
+                  (home-bash-configuration
+                   (aliases '(("grep" . "grep --color=auto")
+                              ("vim" . "nvim")
+                              ("ll" . "ls -l")
+                              ("ls" . "ls -p --color=auto")))
+                   (bashrc (list (local-file ".bashrc" "bashrc")))
+                   (bash-profile (list (local-file ".bash_profile"
+                                                   "bash_profile"))))))))
